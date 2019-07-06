cbb4a6071d1ed28fdeb1af0616c1305584e87716
Inzamam Tahir <inzamamtahir@gmail.com>
2019-07-07 02:40:50 +0500

    users sign up and login apis created

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::UsersController < ApplicationController
+
+  skip_before_action :authenticate_request
+  skip_before_action :verify_authenticity_token
+
+  def create
+    @api_user = User.new(user_params)
+
+    if @api_user.save
+      render json: {
+        auth_token: AuthenticateUser.call(@api_user.email, @api_user.password).result,
+        user: @api_user,
+        success: 'User was successfully created.'
+      } 
+    else
+      render json: { errors: @api_user.errors } 
+    end
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :boards, defaults: {format: :json}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post 'authenticate', to: 'authentication#authenticate', defaults: {format: :json}
+
+  namespace :api do
+    namespace :v1 do
+      resources :boards, defaults: {format: :json}
+
+      # Users routes
+      post 'sign_up', to: 'users#create'
+
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      post 'login', to: 'authentication#authenticate', defaults: {format: :json}
+    end
+  end
+
 end
